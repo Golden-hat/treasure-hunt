bed4d2c57ef267c66aa63708a7b945dbb6f76d07
Golden-hat <yassinpellicerlamla@gmail.com>
2024-11-07 06:39:25 +0100

    satisfecho

--- components/map.jsx
+++ components/map.jsx
@@ -73,8 +73,9 @@ const BrowseEventHandler = ({ focus, hunts, fetchHunts }) => {
   const [expand, setExpand] = useState(false);
 
   useEffect(() => {
-    let aux = [];
     const fetch_hunts = async () => {
+      let aux = [];
+
       const res = await fetch("/api/get_hunts", {
         method: "GET",
         headers: {
@@ -83,40 +84,37 @@ const BrowseEventHandler = ({ focus, hunts, fetchHunts }) => {
       });
       const hunt_data = await res.json();
 
-      hunt_data.hunts.forEach((hunt_) => {
-        let h = new Hunt(
-          hunt_.name,
-          [],
-          hunt_.id,
-          hunt_.description
-        );
-        
-        const fetch_checkpoints = async (id) => {
-          const res = await fetch(`/api/get_checkpoints?id=${id}`, {
+      await Promise.all(
+        hunt_data.hunts.map(async (hunt_) => {
+          let h = new Hunt(
+            hunt_.name,
+            [],
+            hunt_.id,
+            hunt_.description
+          );
+
+          const res = await fetch(`/api/get_checkpoints?id=${hunt_.id}`, {
             method: "GET",
             headers: {
               "Content-Type": "application/json",
             },
           });
-          const checkpoint_data = await res.json()
-          
-          checkpoint_data.checkpoints.forEach((checkpoint) => {
-            let newCheckpoint = {
-              order: checkpoint.order,
-              name: checkpoint.name,
-              description: checkpoint.description,
-              position: [checkpoint.position_lat, checkpoint.position_lng]
-            };
-            h.checkpoints = [... h.checkpoints, newCheckpoint];
-          });
-        };
+          const checkpoint_data = await res.json();
 
-        fetch_checkpoints(hunt_.id);
+          h.checkpoints = checkpoint_data.checkpoints.map((checkpoint) => ({
+            order: checkpoint.order,
+            name: checkpoint.name,
+            description: checkpoint.description,
+            position: [checkpoint.position_lat, checkpoint.position_lng],
+          }));
 
-        aux = [...aux, h]
-      });
+          aux.push(h);
+        })
+      );
+
       fetchHunts(aux);
     };
+
     fetch_hunts();
   }, []);
 
@@ -145,84 +143,84 @@ const BrowseEventHandler = ({ focus, hunts, fetchHunts }) => {
   return (
     <>
       {hunts.map((hunt, index) => (
-        <Marker
-          key={index}
-          position={hunt.checkpoints[0].position}
-          icon={createCustomIcon(hunt.name)}
-          draggable={false}
-          eventHandlers={{
-            dragend: (e) => handleMarkerDragEnd(index, e),
-          }}
-        >
-          <Popup offset={[0, -40]} maxWidth={600}>
-            {/* HUNT PREVIEW */}
-            <div
-              className={`overflow-auto h-[380px] mb-5 rounded-2xl bg-[#e6e6e6] px-2 pt-6 ${
-                expand ? "h-auto" : "max-h-[380px]"
-              }`}
-            >
-              <div className="flex flex-col justify-center items-center mb-4">
-                <h1 className="font-caveat font-bold text-3xl text-left px-6 mb-6">
-                  Details Preview
-                </h1>
-                <div className="cursor-pointer hover:bg-[#b6b6b6] bg-[#d6d6d6] rounded-full p-20 mb-2">
-                  <img
-                    src="/add.svg"
-                    alt="Description of image"
-                    className="scale-[3]"
-                  />
-                </div>
-                <p className="text-xs mb-2">Click to add a banner!</p>
-                <div className="flex flex-col items-center max-w-[80%]">
-                  <h1 className="font-caveat text-center text-5xl bold"></h1>
-                  <h1 className="font-caveat text-center text-2xl italic bold text-gray-600">
-                    By
-                  </h1>
-                </div>
-              </div>
-              <Quill
-                readOnly={true}
-                modules={{ toolbar: false }}
-                style={{
-                  height: "fit-content",
-                  margin: "20px",
-                }}
-                value={hunt.description}
-              ></Quill>
-              <div className="flex flex-col mx-5">
-                <label className="text-lg mb-2">
-                  Difficulty: <span className="font-bold"> </span>
-                </label>
-                <label className="text-sm">
-                  On a scale from 0 to 100! The higher the number, the harder
-                  the challenge.
-                </label>
-              </div>
-              <button
-                className="flex justify-center mx-auto mb-5 sticky bottom-2"
-                onClick={() => setExpand(!expand)}
+        hunt.checkpoints[0] ? (
+          <Marker
+            key={index}
+            position={hunt.checkpoints[0].position}
+            icon={createCustomIcon(hunt.name)}
+            draggable={false}
+          >
+            <Popup offset={[0, -40]} maxWidth={600}>
+              <div
+                className={`overflow-auto h-[380px] mb-5 rounded-2xl bg-[#e6e6e6] px-2 pt-6 ${
+                  expand ? "h-auto" : "max-h-[380px]"
+                }`}
               >
-                {!expand ? (
-                  <img
-                    src="/arrow.svg"
-                    alt="Description of image"
-                    className="cursor-pointer scale-[1] p-2"
-                  />
-                ) : (
-                  <img
-                    src="/arrow.svg"
-                    alt="Description of image"
-                    className="rotate-180 cursor-pointer scale-[1] p-2"
-                  />
-                )}
-              </button>
-            </div>
-          </Popup>
-        </Marker>
+                <div className="flex flex-col justify-center items-center mb-4">
+                  <h1 className="font-caveat font-bold text-3xl text-left px-6 mb-6">
+                    Details Preview
+                  </h1>
+                  <div className="cursor-pointer hover:bg-[#b6b6b6] bg-[#d6d6d6] rounded-full p-20 mb-2">
+                    <img
+                      src="/add.svg"
+                      alt="Description of image"
+                      className="scale-[3]"
+                    />
+                  </div>
+                  <p className="text-xs mb-2">Click to add a banner!</p>
+                  <div className="flex flex-col items-center max-w-[80%]">
+                    <h1 className="font-caveat text-center text-5xl bold"></h1>
+                    <h1 className="font-caveat text-center text-2xl italic bold text-gray-600">
+                      By
+                    </h1>
+                  </div>
+                </div>
+                <Quill
+                  readOnly={true}
+                  modules={{ toolbar: false }}
+                  style={{
+                    maxHeight: !expand ? "fit-content" : "450px",
+                    overflowY: "auto",
+                    margin: "20px",
+                  }}
+                  value={hunt.description}
+                ></Quill>
+                <div className="flex flex-col mx-5">
+                  <label className="text-lg mb-2">
+                    Difficulty: <span className="font-bold"> </span>
+                  </label>
+                  <label className="text-sm">
+                    On a scale from 0 to 100! The higher the number, the harder
+                    the challenge.
+                  </label>
+                </div>
+                <button
+                  className="flex justify-center mx-auto mb-5 sticky bottom-2"
+                  onClick={() => setExpand(!expand)}
+                >
+                  {!expand ? (
+                    <img
+                      src="/arrow.svg"
+                      alt="Description of image"
+                      className="cursor-pointer scale-[1] p-2"
+                    />
+                  ) : (
+                    <img
+                      src="/arrow.svg"
+                      alt="Description of image"
+                      className="rotate-180 cursor-pointer scale-[1] p-2"
+                    />
+                  )}
+                </button>
+              </div>
+            </Popup>
+          </Marker>
+        ) : null
       ))}
     </>
   );
 };
+
 
 const CreateEventsHandler = ({ checkpoints, fetchCheckpoints, focus }) => {
   const map = useMap();
